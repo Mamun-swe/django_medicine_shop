2b28b19a3725026c63cfb2c96867885826df5e7e
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-06-16 22:53:26 +0600

    okk

--- clientapp/views.py
+++ clientapp/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request, "client/home.html")
+    products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    return render(request, "client/home.html", {'data': products})
 
 
 def about(request):
